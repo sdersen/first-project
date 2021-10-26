20306fd4aa6571665853dc1b7751a74db03c3c7c
Sofia Dersén <Sofia_dersen@hotmail.com>
2021-10-26 11:36:16 +0200

    Changed LinkedIn icon and Insta adress

--- theStarWarsProject/about.php
+++ theStarWarsProject/about.php
@@ -2,14 +2,14 @@
 require __DIR__ . '/header.php'; ?>
 
 <div class="aboutContainer">
-    <a href="instagram.com">
+    <a href="https://instagram.com">
         <img class="someImage" src="https://cdn-icons-png.flaticon.com/512/2111/2111463.png" alt="">
     </a>
     <a href="https://github.com/sdersen">
         <img class="someImage" src="https://cdn-icons-png.flaticon.com/512/733/733553.png" alt="">
     </a>
     <a href="https://www.linkedin.com/in/sofia-ders%C3%A9n-020a1325/">
-        <img class="someImage" src="https://cdn-icons.flaticon.com/png/512/3536/premium/3536505.png?token=exp=1634283261~hmac=9a87fbee90db353561a0fd4b3de10f22" alt="">
+        <img class="someImage" src="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Linkedin_unofficial_colored_svg-512.png" alt="">
     </a>
 </div>
 
