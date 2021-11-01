569f2a5979b6dcfcb897a802d9d1ca9ffd08970b
Sofia Dersén <Sofia_dersen@hotmail.com>
2021-11-01 15:36:26 +0100

    Last fix

--- theStarWarsProject/about.php
+++ theStarWarsProject/about.php
@@ -3,13 +3,13 @@ require __DIR__ . '/header.php'; ?>
 
 <div class="aboutContainer">
     <a href="https://instagram.com">
-        <img class="someImage" src="https://cdn-icons-png.flaticon.com/512/2111/2111463.png" alt="">
+        <img class="someImage" src="https://cdn-icons-png.flaticon.com/512/2111/2111463.png" alt="Instagram logo">
     </a>
     <a href="https://github.com/sdersen">
-        <img class="someImage" src="https://cdn-icons-png.flaticon.com/512/733/733553.png" alt="">
+        <img class="someImage" src="https://cdn-icons-png.flaticon.com/512/733/733553.png" alt="Github logo">
     </a>
     <a href="https://www.linkedin.com/in/sofia-ders%C3%A9n-020a1325/">
-        <img class="someImage" src="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Linkedin_unofficial_colored_svg-512.png" alt="">
+        <img class="someImage" src="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Linkedin_unofficial_colored_svg-512.png" alt="LinkedIn logo">
     </a>
 </div>
 
